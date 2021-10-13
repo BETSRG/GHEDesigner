c87ba66b550438d01ffdacd589b328d7cf7aad85
JackCook <jack.cook@okstate.edu>
2021-10-13 15:46:36 -0500

    sizing examples for various BHE models

---
+++ GLHEDT/examples/GLHE/Sizing/HybridGLHE_Double_U_Tube.py
@@ -0,0 +1,136 @@
+# Jack C. Cook
+# Wednesday, October 13, 2021
+
+import GLHEDT.PLAT as PLAT
+import matplotlib.pyplot as plt
+import pandas as pd
+import GLHEDT.PLAT.pygfunction as gt
+import gFunctionDatabase as gfdb
+import GLHEDT
+from time import time as clock
+
+
+def main():
+    # --------------------------------------------------------------------------
+
+    # Borehole dimensions
+    # -------------------
+    H = 100.  # Borehole length (m)
+    D = 2.  # Borehole buried depth (m)
+    r_b = 150. / 1000. / 2.  # Borehole radius]
+    B = 5.  # Borehole spacing (m)
+
+    # Pipe dimensions
+    # ---------------
+    r_out = 26.67 / 1000. / 2.  # Pipe outer radius (m)
+    r_in = 21.6 / 1000. / 2.  # Pipe inner radius (m)
+    s = 32.3 / 1000.  # Inner-tube to inner-tube Shank spacing (m)
+    epsilon = 1.0e-6  # Pipe roughness (m)
+
+    # Pipe positions
+    # --------------
+    # Double U-tube
+    pos = PLAT.media.Pipe.place_pipes(s, r_out, 2)
+    # Double U-tube bhe object
+    bhe_object = PLAT.borehole_heat_exchangers.MultipleUTube
+
+    # Thermal conductivities
+    # ----------------------
+    k_p = 0.4  # Pipe thermal conductivity (W/m.K)
+    k_s = 2.0  # Ground thermal conductivity (W/m.K)
+    k_g = 1.0  # Grout thermal conductivity (W/m.K)
+
+    # Volumetric heat capacities
+    # --------------------------
+    rhoCp_p = 1542. * 1000.  # Pipe volumetric heat capacity (J/K.m3)
+    rhoCp_s = 2343.493 * 1000.  # Soil volumetric heat capacity (J/K.m3)
+    rhoCp_g = 3901. * 1000.  # Grout volumetric heat capacity (J/K.m3)
+
+    # Thermal properties
+    # ------------------
+    # Pipe
+    pipe = PLAT.media.Pipe(pos, r_in, r_out, s, epsilon, k_p, rhoCp_p)
+    # Soil
+    ugt = 18.3  # Undisturbed ground temperature (degrees Celsius)
+    soil = PLAT.media.Soil(k_s, rhoCp_s, ugt)
+    # Grout
+    grout = PLAT.media.ThermalProperty(k_g, rhoCp_g)
+
+    # Number in the x and y
+    # ---------------------
+    N = 12
+    M = 13
+    configuration = 'rectangle'
+    # GFunction
+    # ---------
+    # Access the database for specified configuration
+    r = gfdb.Management.retrieval.Retrieve(configuration)
+    # There is just one value returned in the unimodal domain for rectangles
+    r_unimodal = r.retrieve(N, M)
+    key = list(r_unimodal.keys())[0]
+    print('The key value: {}'.format(key))
+    r_data = r_unimodal[key]
+
+    # Configure the database data for input to the goethermal GFunction object
+    geothermal_g_input = gfdb.Management. \
+        application.GFunction.configure_database_file_for_usage(r_data)
+
+    # Initialize the GFunction object
+    GFunction = gfdb.Management.application.GFunction(**geothermal_g_input)
+
+    # Inputs related to fluid
+    # -----------------------
+    V_flow_system = 31.2  # System volumetric flow rate (L/s)
+    mixer = 'MEG'  # Ethylene glycol mixed with water
+    percent = 0.  # Percentage of ethylene glycol added in
+    # Fluid properties
+    fluid = gt.media.Fluid(mixer=mixer, percent=percent)
+
+    # Define a borehole
+    borehole = gt.boreholes.Borehole(H, D, r_b, x=0., y=0.)
+
+    # Simulation start month and end month
+    # --------------------------------
+    # Simulation start month and end month
+    start_month = 1
+    n_years = 20
+    end_month = n_years * 12
+    # Maximum and minimum allowable fluid temperatures
+    max_EFT_allowable = 35  # degrees Celsius
+    min_EFT_allowable = 5  # degrees Celsius
+    # Maximum and minimum allowable heights
+    max_Height = 150  # in meters
+    min_Height = 60  # in meters
+    sim_params = PLAT.media.SimulationParameters(
+        start_month, end_month, max_EFT_allowable, min_EFT_allowable,
+        max_Height, min_Height)
+
+    # Process loads from file
+    # -----------------------
+    # read in the csv file and convert the loads to a list of length 8760
+    hourly_extraction: dict = \
+        pd.read_csv('../Atlanta_Office_Building_Loads.csv').to_dict('list')
+    # Take only the first column in the dictionary
+    hourly_extraction_ground_loads: list = \
+        hourly_extraction[list(hourly_extraction.keys())[0]]
+
+    # --------------------------------------------------------------------------
+
+    # Initialize Hybrid GLHE object
+    HybridGLHE = GLHEDT.ground_heat_exchangers._HybridGLHE(
+        V_flow_system, B, bhe_object, fluid, borehole, pipe, grout, soil,
+        GFunction, sim_params, hourly_extraction_ground_loads)
+
+    HybridGLHE.size()
+
+    print(HybridGLHE.bhe.b.H)
+
+    GLHE_info = HybridGLHE.__repr__()
+
+    file = open('DoubleUTube-HybridGLHE-info.txt', 'w+')
+    file.write(GLHE_info)
+    file.close()
+
+
+if __name__ == '__main__':
+    main()
