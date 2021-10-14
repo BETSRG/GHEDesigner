e9ff47c792d03801b465e232adddb07690808b69
j-c-cook <jack.cook@okstate.edu>
2021-10-14 09:34:13 -0500

    add plots to simulation example

--- GHEDT/examples/GHE/HybridGHESizing/simulation.py
+++ GHEDT/examples/GHE/HybridGHESizing/simulation.py
@@ -127,6 +127,53 @@ def main():
 
     print('Min EFT: {}\nMax EFT: {}'.format(min_HP_EFT, max_HP_EFT))
 
+    # Plot the simulation results
+    # ---------------------------
+    fig, ax = plt.subplots()
+
+    heat_pump_EFT = HybridGHE.HPEFT[2:]
+    months = range(1, len(heat_pump_EFT) + 1)
+
+    min_HP_EFT_idx = HybridGHE.HPEFT.index(min_HP_EFT) - 1
+    max_HP_EFT_idx = HybridGHE.HPEFT.index(max_HP_EFT) - 1
+
+    ax.plot(months, heat_pump_EFT, 'k')
+    ax.scatter(min_HP_EFT_idx, min_HP_EFT, color='b', marker='X', s=200,
+               label='Minimum Temperature')
+    ax.scatter(max_HP_EFT_idx, max_HP_EFT, color='r', marker='P', s=200,
+               label='Maximum Temperature')
+
+    ax.set_xlabel('Month number')
+    ax.set_ylabel('Heat pump entering fluid temperature ($\degree$C)')
+
+    ax.grid()
+    ax.set_axisbelow(True)
+
+    fig.legend(bbox_to_anchor=(.5, .95))
+
+    fig.tight_layout()
+
+    fig.savefig('hybrid_monthly_simulation.png')
+
+    # Plot the hourly load profile
+    # ---------------------
+    fig = HybridGHE.hybrid_load.visualize_hourly_heat_extraction()
+
+    fig.savefig('Atlanta_Office_Building_extraction_loads.png')
+
+    # Plot the hybrid load representation
+    # -----------------------------------
+    fig, ax = plt.subplots()
+
+    ax.plot(HybridGHE.hybrid_load.load)
+
+    ax.set_xlabel('Month number')
+    ax.set_ylabel('Monthly load (kW)')
+
+    fig.tight_layout()
+
+    fig.savefig('monthly_load_representation.png')
+
 
 if __name__ == '__main__':
     main()
