92cb34237826a85388aeebb4e675f3ef6e517b91
j-c-cook <jack.cook@okstate.edu>
2021-10-23 14:22:42 -0500

    update zr 2

--- GHEDT/examples/Stanford_Conference/zoned_rectangle_2.py
+++ GHEDT/examples/Stanford_Conference/zoned_rectangle_2.py
@@ -1,12 +1,12 @@
 # Jack C. Cook
 # Friday, September 17, 2021
 
-import PLAT
+import GHEDT.PLAT as PLAT
+import GHEDT.PLAT.pygfunction as gt
 import matplotlib.pyplot as plt
 import pandas as pd
-import PLAT.pygfunction as gt
 import gFunctionDatabase as gfdb
-import GLHEDT
+import GHEDT
 
 
 def main():
@@ -30,6 +30,8 @@ def main():
     # --------------
     # Single U-tube [(x_in, y_in), (x_out, y_out)]
     pos = PLAT.media.Pipe.place_pipes(s, r_out, 1)
+    # Single U-tube BHE object
+    bhe_object = PLAT.borehole_heat_exchangers.SingleUTube
 
     # Thermal conductivities
     # ----------------------
@@ -45,9 +47,13 @@ def main():
 
     # Thermal properties
     # ------------------
-    pipe = PLAT.media.Pipe(pos, r_in, r_out, s, epsilon, k_p, rhoCp=rhoCp_p)
-    soil = PLAT.media.ThermalProperty(k=k_s, rhoCp=rhoCp_s)
-    grout = PLAT.media.ThermalProperty(k=k_g, rhoCp=rhoCp_g)
+    # Pipe
+    pipe = PLAT.media.Pipe(pos, r_in, r_out, s, epsilon, k_p, rhoCp_p)
+    # Soil
+    ugt = 18.3  # Undisturbed ground temperature (degrees Celsius)
+    soil = PLAT.media.Soil(k_s, rhoCp_s, ugt)
+    # Grout
+    grout = PLAT.media.ThermalProperty(k_g, rhoCp_g)
 
     # Number in the x and y
     # ---------------------
@@ -56,11 +62,37 @@ def main():
     configuration = 'zoned'
     nbh = N * M
 
+    # GFunction
+    # ---------
+    # Access the database for specified configuration
+    z = gfdb.Management.retrieval.Retrieve(configuration)
+    # There is just one value returned in the unimodal domain for rectangles
+    z_unimodal = z.retrieve(N, M)
+    key = list(z_unimodal.keys())[-5]
+    print('The key value: {}'.format(key))
+    r_data = z_unimodal[key]
+    key_split = key.split('_')
+    Ni, Mi = key_split[len(key_split) - 2:len(key_split)]
+    Ni = int(Ni)
+    Mi = int(Mi)
+
+    # Configure the database data for input to the goethermal GFunction object
+    geothermal_g_input = gfdb.Management. \
+        application.GFunction.configure_database_file_for_usage(r_data)
+
+    # Initialize the GFunction object
+    GFunction = gfdb.Management.application.GFunction(**geothermal_g_input)
+
     # Inputs related to fluid
     # -----------------------
-    V_flow_system = 15.  # System volumetric flow rate (L/s)
+    V_flow_system = 18.2  # System volumetric flow rate (L/s)
     mixer = 'MEG'  # Ethylene glycol mixed with water
     percent = 0.  # Percentage of ethylene glycol added in
+    # Fluid properties
+    fluid = gt.media.Fluid(mixer=mixer, percent=percent)
+
+    # Define a borehole
+    borehole = gt.boreholes.Borehole(H, D, r_b, x=0., y=0.)
 
     # Simulation start month and end month
     # ------------------------------------
@@ -73,8 +105,11 @@ def main():
     max_EFT_allowable = 35  # degrees Celsius
     min_EFT_allowable = 5  # degrees Celsius
     # Maximum and minimum allowable heights
-    max_Height = 350  # in meters
+    max_Height = 280  # in meters
     min_Height = 50  # in meters
+    sim_params = PLAT.media.SimulationParameters(
+        start_month, end_month, max_EFT_allowable, min_EFT_allowable,
+        max_Height, min_Height)
 
     # Process loads from file
     # -----------------------
@@ -82,87 +117,35 @@ def main():
     hourly_extraction: dict = \
         pd.read_csv('Atlanta_Office_Building_Loads.csv').to_dict('list')
     # Take only the first column in the dictionary
-    hourly_extraction_loads: list = \
+    hourly_extraction_ground_loads: list = \
         hourly_extraction[list(hourly_extraction.keys())[0]]
 
     # --------------------------------------------------------------------------
 
-    # Borehole heat exchanger
-    # -----------------------
-    # Fluid properties
-    fluid = gt.media.Fluid(mixer=mixer, percent=percent)
-    # Volumetric flow rate per borehole (L/s)
-    V_flow_borehole = V_flow_system / nbh
-    # Total fluid mass flow rate per borehole (kg/s)
-    m_flow_borehole = V_flow_borehole / 1000. * fluid.rho
+    # Initialize Hourly GLHE object
+    GHE = GHEDT.ground_heat_exchangers.GHE(
+        V_flow_system, B, bhe_object, fluid, borehole, pipe, grout, soil,
+        GFunction, sim_params, hourly_extraction_ground_loads)
 
-    # Define a borehole
-    borehole = gt.boreholes.Borehole(H, D, r_b, x=0., y=0.)
+    GHE.size(method='hybrid')
 
-    single_u_tube = PLAT.borehole_heat_exchangers.SingleUTube(
-        m_flow_borehole, fluid, borehole, soil, grout, pipe)
+    print('The sized height: {}'.format(GHE.bhe.b.H))
 
-    # Radial Numerical short time step g-function
-    # -------------------------------------------
-    # Compute short time step now that the BHE is defined
-    # Compute short time step
-    radial_numerical = \
-        PLAT.radial_numerical_borehole.RadialNumericalBH(single_u_tube)
-
-    radial_numerical.calc_sts_g_functions(single_u_tube)
-
-    # Hybrid load
-    # -----------
-    # Split the extraction loads into heating and cooling for input to the
-    # HybridLoad object
-    hourly_rejection_loads, hourly_extraction_loads = \
-        PLAT.ground_loads.HybridLoad.split_heat_and_cool(
-            hourly_extraction_loads)
-
-    hybrid_load = PLAT.ground_loads.HybridLoad(
-        hourly_rejection_loads, hourly_extraction_loads, single_u_tube,
-        radial_numerical, start_month, end_month)
-
-    # GFunction
-    # ---------
-    # Access the database for specified configuration
-    r = gfdb.Management.retrieval.Retrieve(configuration)
-    # There is just one value returned in the unimodal domain for rectangles
-    r_unimodal = r.retrieve(N, M)
-    key = list(r_unimodal.keys())[-2]
-    print('The key value: {}'.format(key))
-    r_data = r_unimodal[key]
-
-    # Configure the database data for input to the goethermal GFunction object
-    geothermal_g_input = \
-        GLHEDT.geothermal.GFunction.configure_database_file_for_usage(r_data)
-
-    # Initialize the GFunction object
-    GFunction = GLHEDT.geothermal.GFunction(**geothermal_g_input)
-
-    # Hybrid GLHE
-    # -----------
-    # Initialize a HybridGLHE
-    HybridGLHE = GLHEDT.ground_heat_exchangers.HybridGLHE(single_u_tube,
-                                                          radial_numerical,
-                                                          hybrid_load,
-                                                          GFunction)
-    HybridGLHE.size(
-        max_Height, min_Height, max_EFT_allowable, min_EFT_allowable, B=B)
-
-    print('The sized height: {}'.format(single_u_tube.b.H))
+    print('The sized height: {}'.format(GHE.bhe.b.H))
     print('Number of boreholes: {}'.format(len(GFunction.bore_locations)))
-    print('Total depth: {}'.format(single_u_tube.b.H * len(GFunction.bore_locations)))
+    print('Total depth: {}'.format(GHE.bhe.b.H * len(GFunction.bore_locations)))
 
     # Plot go and no-go zone with corrected borefield
     # -----------------------------------------------
-    new_coordinates = GFunction.correct_coordinates(B)
+    zr = gfdb.coordinates.ZonedRectangle(M, N, B, Nix=Mi, Niy=Ni)
+    zr.gen_z_rect()
+    coordinates = list(zr.coordinates.values())[0]
 
     perimeter = [[0., 0.], [70.104, 0.], [70.104, 80.772], [0., 80.772]]
     no_go = [[9.997, 36.51], [9.997, 69.79], [59.92, 69.79], [59.92, 36.51]]
 
     fig, ax = GFunction.visualize_area_and_constraints(perimeter,
-                                                       new_coordinates,
+                                                       coordinates,
                                                        no_go=no_go)
 
     fig.savefig('zoned_rectangle_case_2.png')
@@ -171,7 +154,7 @@ def main():
     # -------------------------------
     fig, ax = GFunction.visualize_g_functions()
 
-    B_over_H = B / single_u_tube.b.H
+    B_over_H = B / GHE.bhe.b.H
     # interpolate for the Long time step g-function
     g_function, rb_value, D_value, H_eq = \
         GFunction.g_function_interpolation(B_over_H)
@@ -179,7 +162,7 @@ def main():
     g_function_corrected = \
         GFunction.borehole_radius_correction(g_function,
                                              rb_value,
-                                             single_u_tube.b.r_b)
+                                             GHE.bhe.b.r_b)
     ax.plot(GFunction.log_time, g_function_corrected, '--')
 
     fig.savefig('zoned_rectangle_case_2_gFunctions.png')
