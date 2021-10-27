c734dddc6af7994357517b3fdfb2614a2d1faf1e
j-c-cook <jack.cook@okstate.edu>
2021-10-27 17:02:55 -0500

    add error handling to square and near square

--- ghedt/domains.py
+++ ghedt/domains.py
@@ -7,9 +7,16 @@ import ghedt
 def square_and_near_square(lower: int,
                            upper: int,
                            B: float):
+    if lower or upper <= 0:
+        raise ValueError('The lower and upper arguments must be positive'
+                         'integer values.')
+    if upper < lower:
+        raise ValueError('The lower argument should be less than or equal to'
+                         'the upper.')
+
     coordinates_domain = []
 
-    for i in range(lower, upper):
+    for i in range(lower, upper+1):
         for j in range(2):
             coordinates = \
                 ghedt.coordinates.rectangle(i, i+j, B, B)
