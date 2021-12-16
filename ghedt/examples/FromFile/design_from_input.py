b096647e7b806744d775d544c3f7c3baa2fc7792
j_c_cook <jack.cook@okstate.edu>
2021-12-16 07:41:40 -1000

    update notation in from file design

--- ghedt/examples/FromFile/design_from_input.py
+++ ghedt/examples/FromFile/design_from_input.py
@@ -8,18 +8,19 @@ def main():
     # Note: The create_near_square_input_file.py needs to be run to create an
     # input file.
 
-    # Enter the path to the input file
+    # Enter the path to the input file named `ghedt_input.obj`.
     path_to_file = 'ghedt_input.obj'
     # Initialize a Design object that is based on the content of the
-    # path_to_file variable
+    # path_to_file variable.
     design = dt.design.read_input_file(path_to_file)
-    # Find the design based on the inputs
+    # Find the design based on the inputs.
     bisection_search = design.find_design()
-    # Perform sizing in between the min and max bounds
+    # Perform sizing in between the min and max bounds.
     ghe = bisection_search.ghe
     ghe.compute_g_functions()
     ghe.size(method='hybrid')
-    # Export the g-function to
+    # Export the g-function to a file named `ghedt_output`. A json file will be
+    # created.
     dt.design.oak_ridge_export(bisection_search, file_name='ghedt_output')
 
 
