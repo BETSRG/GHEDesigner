85964332919d0b0def63050c13577bf3db11754b
Matt Mitchell <matt.mitchell@nrel.gov>
2022-12-11 19:09:00 -0700

    split up test_find_near_square into three unit tests instead of one big one

--- ghedesigner/tests/test_find_near_square.py
+++ ghedesigner/tests/test_find_near_square.py
@@ -14,11 +14,10 @@ from ghedesigner.output import output_design_details
 from ghedesigner.tests.ghe_base_case import GHEBaseTest
 from ghedesigner.utilities import DesignMethod, length_of_side
 
-from ghedesigner.manager import GHEManager
-
 
 class TestFindNearSquare(GHEBaseTest):
-    def test_find_near_square(self):
+
+    def test_find_near_square_single(self):
         # This file contains 3 examples utilizing the square-near-square design algorithm for a single U, double U, and
         # coaxial tube  The results from these examples are exported to the "DesignExampleOutput" folder.
 
@@ -158,8 +157,79 @@ class TestFindNearSquare(GHEBaseTest):
             load_method=DesignMethod.Hybrid,
         )
 
+    def test_find_near_square_double(self):
         # *************************************************************************************************************
         # Double U-tube Example
+
+        project_name = "Atlanta Office Building: Design Example"
+        author = "John Doe"
+        iteration_name = "Example 1"
+        output_file_directory = self.test_outputs_directory / "DesignExampleOutput"
+
+        # Borehole dimensions
+        h = 96.0  # Borehole length (m)
+        d = 2.0  # Borehole buried depth (m)
+        r_b = 0.075  # Borehole radius (m)
+        b = 5.0  # Borehole spacing (m)
+
+        # Single and Multiple U-tube Pipe Dimensions
+        r_out = 26.67 / 1000.0 / 2.0  # Pipe outer radius (m)
+        r_in = 21.6 / 1000.0 / 2.0  # Pipe inner radius (m)
+        s = 32.3 / 1000.0  # Inner-tube to inner-tube Shank spacing (m)
+        epsilon = 1.0e-6  # Pipe roughness (m)
+
+        # Single U Tube Pipe Positions
+        pos_single = Pipe.place_pipes(s, r_out, 1)
+
+        # Thermal conductivities
+        k_p = 0.4  # Pipe thermal conductivity (W/m.K)
+        k_s = 2.0  # Ground thermal conductivity (W/m.K)
+        k_g = 1.0  # Grout thermal conductivity (W/m.K)
+
+        # Volumetric heat capacities
+        rho_cp_p = 1542.0 * 1000.0  # Pipe volumetric heat capacity (J/K.m3)
+        rho_cp_s = 2343.493 * 1000.0  # Soil volumetric heat capacity (J/K.m3)
+        rho_cp_g = 3901.0 * 1000.0  # Grout volumetric heat capacity (J/K.m3)
+
+        # Instantiating Soil Properties
+        ugt = 18.3  # Undisturbed ground temperature (degrees Celsius)
+        soil = Soil(k_s, rho_cp_s, ugt)
+
+        # Instantiating Grout Properties
+        grout = Grout(k_g, rho_cp_g)
+
+        # Fluid properties
+        fluid = GHEFluid(fluid_str="Water", percent=0.0)
+
+        # Fluid Flow Properties
+        v_flow = 0.2  # Volumetric flow rate (L/s)
+        # Note: The flow parameter can be borehole or system.
+        flow = "borehole"
+
+        # Instantiate a Borehole
+        borehole = GHEBorehole(h, d, r_b, x=0.0, y=0.0)
+
+        # Simulation parameters
+        start_month = 1
+        n_years = 20
+        end_month = n_years * 12
+        max_eft_allowable = 35  # degrees Celsius (HP EFT)
+        min_eft_allowable = 5  # degrees Celsius (HP EFT)
+        max_height = 135.0  # in meters
+        min_height = 60  # in meters
+        sim_params = SimulationParameters(
+            start_month,
+            end_month,
+            max_eft_allowable,
+            min_eft_allowable,
+            max_height,
+            min_height,
+        )
+
+        number_of_boreholes = 32
+        length = length_of_side(number_of_boreholes, b)
+        geometric_constraints = GeometricConstraints(b=b, length=length)
+        hourly_extraction_ground_loads = self.get_atlanta_loads()
 
         note = "Square-Near-Square Usage Example: Double U Tube"
 
@@ -220,8 +290,79 @@ class TestFindNearSquare(GHEBaseTest):
             load_method=DesignMethod.Hybrid,
         )
 
+    def test_find_near_square_coaxial(self):
         # *************************************************************************************************************
         # Coaxial Tube Example
+
+        project_name = "Atlanta Office Building: Design Example"
+        author = "John Doe"
+        iteration_name = "Example 1"
+        output_file_directory = self.test_outputs_directory / "DesignExampleOutput"
+
+        # Borehole dimensions
+        h = 96.0  # Borehole length (m)
+        d = 2.0  # Borehole buried depth (m)
+        r_b = 0.075  # Borehole radius (m)
+        b = 5.0  # Borehole spacing (m)
+
+        # Single and Multiple U-tube Pipe Dimensions
+        r_out = 26.67 / 1000.0 / 2.0  # Pipe outer radius (m)
+        r_in = 21.6 / 1000.0 / 2.0  # Pipe inner radius (m)
+        s = 32.3 / 1000.0  # Inner-tube to inner-tube Shank spacing (m)
+        epsilon = 1.0e-6  # Pipe roughness (m)
+
+        # Single U Tube Pipe Positions
+        pos_single = Pipe.place_pipes(s, r_out, 1)
+
+        # Thermal conductivities
+        k_p = 0.4  # Pipe thermal conductivity (W/m.K)
+        k_s = 2.0  # Ground thermal conductivity (W/m.K)
+        k_g = 1.0  # Grout thermal conductivity (W/m.K)
+
+        # Volumetric heat capacities
+        rho_cp_p = 1542.0 * 1000.0  # Pipe volumetric heat capacity (J/K.m3)
+        rho_cp_s = 2343.493 * 1000.0  # Soil volumetric heat capacity (J/K.m3)
+        rho_cp_g = 3901.0 * 1000.0  # Grout volumetric heat capacity (J/K.m3)
+
+        # Instantiating Soil Properties
+        ugt = 18.3  # Undisturbed ground temperature (degrees Celsius)
+        soil = Soil(k_s, rho_cp_s, ugt)
+
+        # Instantiating Grout Properties
+        grout = Grout(k_g, rho_cp_g)
+
+        # Fluid properties
+        fluid = GHEFluid(fluid_str="Water", percent=0.0)
+
+        # Fluid Flow Properties
+        v_flow = 0.2  # Volumetric flow rate (L/s)
+        # Note: The flow parameter can be borehole or system.
+        flow = "borehole"
+
+        # Instantiate a Borehole
+        borehole = GHEBorehole(h, d, r_b, x=0.0, y=0.0)
+
+        # Simulation parameters
+        start_month = 1
+        n_years = 20
+        end_month = n_years * 12
+        max_eft_allowable = 35  # degrees Celsius (HP EFT)
+        min_eft_allowable = 5  # degrees Celsius (HP EFT)
+        max_height = 135.0  # in meters
+        min_height = 60  # in meters
+        sim_params = SimulationParameters(
+            start_month,
+            end_month,
+            max_eft_allowable,
+            min_eft_allowable,
+            max_height,
+            min_height,
+        )
+
+        number_of_boreholes = 32
+        length = length_of_side(number_of_boreholes, b)
+        geometric_constraints = GeometricConstraints(b=b, length=length)
+        hourly_extraction_ground_loads = self.get_atlanta_loads()
 
         note = "Square-Near-Square Usage Example: Coaxial Tube"
 
