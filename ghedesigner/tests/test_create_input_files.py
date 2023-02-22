0e6381bb9d6f8f5dc46216caf9b4ef3107428a37
Matt Mitchell <matt.mitchell@nrel.gov>
2023-02-21 22:04:52 -0700

    add test for creating rectangular single u-tube inputs files

--- ghedesigner/tests/test_create_input_files.py
+++ ghedesigner/tests/test_create_input_files.py
@@ -168,7 +168,33 @@ class TestCreateInputFiles(GHEBaseTest):
         demo_file = self.demos_path / 'find_design_rectangular_double_u_tube.json'
         d_demo = loads(demo_file.read_text())
 
-        with open(self.test_outputs_directory / 'find_design_rectangular_double_u_tube.json', 'w') as f:
+        # get new file
+        d_new = loads(out_path.read_text())
+
+        # compare files
+        recursive_eq(d_demo, d_new)
+
+    def test_create_input_file_rectangular_single_u_tube(self):
+        ghe = GHEManager()
+        ghe.set_single_u_tube_pipe(
+            inner_radius=0.0108, outer_radius=0.0133, shank_spacing=0.0323,
+            roughness=1.0e-6, conductivity=0.4, rho_cp=1542000.0)
+        ghe.set_soil(conductivity=2.0, rho_cp=2343493.0, undisturbed_temp=18.3)
+        ghe.set_grout(conductivity=1.0, rho_cp=3901000.0)
+        ghe.set_fluid()
+        ghe.set_borehole(height=96.0, buried_depth=2.0, radius=0.075)
+        ghe.set_simulation_parameters(num_months=240, max_eft=35, min_eft=5, max_height=135, min_height=60)
+        ghe.set_ground_loads_from_hourly_list(self.get_atlanta_loads())
+        ghe.set_geometry_constraints_rectangular(length=100.0, width=100.0, b_min=3.0, b_max=10.0)
+        ghe.set_design(flow_rate=0.2, flow_type="borehole", design_method_geo=ghe.DesignGeomType.Rectangular)
+        out_path = self.test_outputs_directory / "test_create_input_file_rectangular_single_u_tube.json"
+        ghe.write_input_file(out_path)
+
+        # get demo file
+        demo_file = self.demos_path / 'find_design_rectangular_single_u_tube.json'
+        d_demo = loads(demo_file.read_text())
+
+        with open(self.test_outputs_directory / 'find_design_rectangular_single_u_tube.json', 'w') as f:
             f.write(dumps(d_demo, sort_keys=True, indent=2, separators=(',', ': ')))
 
         # get new file
