2b74dd1181f02b6bdfe97251b99dd535defe6176
Matt Mitchell <matt.mitchell@nrel.gov>
2023-03-18 22:21:19 -0600

    format

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+
 # PATCH `sphinx-jsonschema`
 # to render the extra `units`` schema properties
 
