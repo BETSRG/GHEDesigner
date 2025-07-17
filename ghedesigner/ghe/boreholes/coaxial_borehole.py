bd582d730d4bd4b55f345e7d90c3a123d11551a6
Matt Mitchell <matt.mitchell@nrel.gov>
2025-07-17 15:41:38 -0600

    eliminate pyg from coaxial, merge and resolve conflicts

--- ghedesigner/ghe/boreholes/coaxial_borehole.py
+++ ghedesigner/ghe/boreholes/coaxial_borehole.py
@@ -1,6 +1,5 @@
 from typing import cast
 
-import pygfunction as gt
 from bhr.borehole import Borehole as BHRBorehole
 from pygfunction.boreholes import Borehole
 
@@ -11,27 +10,25 @@ from ghedesigner.ghe.pipe import Pipe
 from ghedesigner.media import GHEFluid, Grout, Soil
 
 
-class CoaxialPipe(gt.pipes.Coaxial, GHEDesignerBoreholeWithMultiplePipes):
+class CoaxialPipe(GHEDesignerBoreholeWithMultiplePipes):
     def __init__(
         self, m_flow_borehole: float, fluid: GHEFluid, _borehole: Borehole, pipe: Pipe, grout: Grout, soil: Soil
     ) -> None:
+        super().__init__(m_flow_borehole, fluid, _borehole, pipe, grout, soil)
+
         # Ensure r_in and r_out are list[float] rather than floats
         if not isinstance(pipe.r_out, list) or not isinstance(pipe.r_in, list):
             raise TypeError("pipe r_in and r_out must be list[float]")
 
         # Unpack the radii to reduce confusion in the future
+        # Pipe naming nomenclature
+        # <var>_<inner/outer pipe>_<inner/outer surface>
+        # e.g. r_in_in is inner radius of the inner pipe
         self.r_inner = cast(tuple[float, float], pipe.r_in)
         self.r_outer = cast(tuple[float, float], pipe.r_out)
         self.r_in_in, self.r_in_out = self.r_inner
         self.r_out_in, self.r_out_out = self.r_outer
 
-        self.m_flow_borehole = m_flow_borehole
-        # Store Thermal properties
-        self.soil = soil
-        self.grout = grout
-        self.pipe = pipe
-        # Store fluid properties
-        self.fluid = fluid
         # Store pipe roughness
         self.roughness = self.pipe.roughness
 
@@ -51,98 +48,6 @@ class CoaxialPipe(gt.pipes.Coaxial, GHEDesignerBoreholeWithMultiplePipes):
             fluid_concentration=self.fluid.concentration_percent / 100,
             boundary_condition="UNIFORM_BOREHOLE_WALL_TEMP",
         )
-
-        # Pipe naming nomenclature
-        # <var>_<inner/outer pipe>_<inner/outer surface>
-        # e.g. r_in_in is inner radius of the inner pipe
-
-        self.borehole = _borehole  # pygfunction borehole
-
-        # Declare variables that are computed in compute_resistance()
-        self.R_p_in = 0.0
-        self.R_p_out = 0.0
-        self.R_grout = 0.0
-        self.h_f_in = 0.0
-        self.h_f_a_in = 0.0
-        self.h_f_a_out = 0.0
-        self.R_f_a_in = 0.0
-        self.R_f_a_out = 0.0
-        self.R_f_in = 0.0
-        self.R_fp = 0.0
-
-        # Store Thermal properties
-        self.soil = soil
-        self.grout = grout
-        self.pipe = pipe
-        # Store fluid properties
-        self.fluid = fluid
-        # Store borehole
-        self.borehole = _borehole
-
-        # # compute resistances required to construct inherited class
-        # self.calc_fluid_pipe_resistance()
-
-        # Vectors of inner and outer pipe radii
-        # Note: The dimensions of the inlet pipe are the first elements of the vectors.
-        # In this example, the inlet pipe is the inside pipe.
-        # TODO: fix this
-        # r_in = cast(tuple[float, float], pipe.r_in)
-        # r_out = cast(tuple[float, float], pipe.r_out)
-        # r_inner_p = np.array([r_in[0], r_out[0]])  # Inner pipe radii (m)
-        # r_outer_p = np.array([r_in[1], r_out[1]])  # Outer pipe radii (m)
-
-        # gt.pipes.Coaxial.__init__(
-        #     self, pipe.pos, r_inner_p, r_outer_p, _borehole, self.soil.k, self.grout.k, self.R_ff, self.R_fp
-        # )
-        #
-        # # these methods must be called after inherited class construction
-        # self.update_thermal_resistances(self.R_ff, self.R_fp)
-        self.calc_effective_borehole_resistance()
-
-    # def calc_fluid_pipe_resistance(self):
-    #     # inner pipe convection resistance
-    #     self.h_f_in = gt.pipes.convective_heat_transfer_coefficient_circular_pipe(
-    #         self.m_flow_borehole,
-    #         self.r_in_in,
-    #         self.fluid.mu,
-    #         self.fluid.rho,
-    #         self.fluid.k,
-    #         self.fluid.cp,
-    #         self.pipe.roughness,
-    #     )
-    #     self.R_f_in = self.compute_fluid_resistance(self.h_f_in, self.r_in_in)
-    #
-    #     # inner pipe conduction resistance
-    #     self.R_p_in = gt.pipes.conduction_thermal_resistance_circular_pipe(self.r_in_in, self.r_in_out,
-    #     self.pipe.k[0])
-    #
-    #     # annulus convection resistances
-    #     self.h_f_a_in, self.h_f_a_out = gt.pipes.convective_heat_transfer_coefficient_concentric_annulus(
-    #         self.m_flow_borehole,
-    #         self.r_in_out,
-    #         self.r_out_in,
-    #         self.fluid.mu,
-    #         self.fluid.rho,
-    #         self.fluid.k,
-    #         self.fluid.cp,
-    #         self.roughness,
-    #     )
-    #
-    #     self.R_f_a_in = self.compute_fluid_resistance(self.h_f_a_in, self.r_in_out)
-    #     self.R_f_a_out = self.compute_fluid_resistance(self.h_f_a_out, self.r_out_in)
-    #
-    #     # inner pipe conduction resistance
-    #     self.R_p_out = gt.pipes.conduction_thermal_resistance_circular_pipe(
-    #         self.r_out_in, self.r_out_out, self.pipe.k[1]
-    #     )
-    #
-    #     # inner fluid to inner annulus fluid resistance
-    #     self.R_ff = self.R_f_in + self.R_p_in + self.R_f_a_in
-    #
-    #     # outer annulus fluid to pipe thermal resistance
-    #     self.R_fp = self.R_p_out + self.R_f_a_out
-    #
-    #     return self.R_fp
 
     def calc_effective_borehole_resistance(self) -> float:
         resist_bh_effective = self.bhr_borehole.calc_bh_resist(self.m_flow_borehole, self.soil.ugt)
@@ -202,5 +107,4 @@ class CoaxialPipe(gt.pipes.Coaxial, GHEDesignerBoreholeWithMultiplePipes):
         # Compute volumes for concentric ghe geometry
         vol_fluid = PI * ((r_in_in**2) + (r_out_in**2) - (r_in_out**2))
         vol_pipe = PI * ((r_in_out**2) - (r_in_in**2) + (r_out_out**2) - (r_out_in**2))
-
         return vol_fluid, vol_pipe
