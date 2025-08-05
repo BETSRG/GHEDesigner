d59e903cb58e23d5a08d2446b22ab047218f9a9f
Matt Mitchell <matt.mitchell@nrel.gov>
2025-08-05 16:43:11 -0600

    schema updates, try updating demo file

--- ghedesigner/validate.py
+++ ghedesigner/validate.py
@@ -21,17 +21,20 @@ def validate_input_file(input_file_path: Path) -> None:
         schema = loads(schema_path.read_text())
         validate(instance=instance, schema=schema)
     except ValidationError as error:
-        print("\n Validation Error:")
-        print(f"  Bad Input Location: {' → '.join(map(str, error.path)) if error.path else 'Root'}")
-        print(f"  Problematic Key: {error.path[-1] if error.path else 'N/A'}")
-        print(f"  Error: {error.message}")
+        print("\n Validation Error:", file=sys.stderr)
+        print(f"  Bad Input Location: {' → '.join(map(str, error.path)) if error.path else 'Root'}", file=sys.stderr)
+        print(f"  Problematic Key: {error.path[-1] if error.path else 'N/A'}", file=sys.stderr)
+        print(f"  Error: {error.message}", file=sys.stderr)
         fix = "\n Suggested Fix:"
         if "required" in error.schema and isinstance(error.schema["required"], list):
-            print(f"{fix}  Ensure that the following required keys are present: {', '.join(error.schema['required'])}")
+            print(
+                f"{fix}  Ensure that the following required keys are present: {', '.join(error.schema['required'])}",
+                file=sys.stderr,
+            )
         elif "enum" in error.schema:
-            print(f"{fix}  Use one of the allowed values: {', '.join(map(str, error.schema['enum']))}")
+            print(f"{fix}  Use one of the allowed values: {', '.join(map(str, error.schema['enum']))}", file=sys.stderr)
         elif "type" in error.schema:
-            print(f"{fix}  Ensure the value is of type: {error.schema['type']}")
+            print(f"{fix}  Ensure the value is of type: {error.schema['type']}", file=sys.stderr)
         print("For example inputs, see the demo files.", file=sys.stderr)
         raise
 
