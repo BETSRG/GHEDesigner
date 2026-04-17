6de0ba5f7427b9857b905b1b0a7359583aaba5c2
niranjan-bas <niranjan.bastakoti@okstate.edu>
2026-04-17 01:33:20 -0500

    addition of hybrid timestep solver

--- ghedesigner/runner_code.py
+++ ghedesigner/runner_code.py
@@ -5,7 +5,7 @@ from time import time
 
 def main():
 
-    input_file_path = Path("..\\demos\\simulate_1_pipe_3_ghe_6_bldg_district_HYBRID.json")
+    input_file_path = Path("..\\demos\\simulate_1_pipe_3_ghe_6_bldg_district_HOURLY.json")
     output_file_path = Path("..\\ghedesigner\\ghe\\nbast_results\\simulation_results.csv")
 
     start_time = time()
